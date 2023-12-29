56e87599f430b335e70cd5ba555c348cdd33ab4f
Sandesh <sandeshsutar802@gmail.com>
2023-12-30 01:42:02 +0530

    rendered a html file.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.get('/', (req,res) => {
-    res.send('Hello World');
+    // res.send('Hello World');
+    const filePath = path.resolve(__dirname, 'home.html');
+    res.sendFile(filePath);
 });
 
 app.listen(port, () => {
