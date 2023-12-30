26d4d45f93e892c39458416c0c064e9c0e8ff70e
Sandesh <sandeshsutar802@gmail.com>
2023-12-31 00:55:14 +0530

    got data from html using bodyparser module.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req,res) => {
     // res.send('Hello World');
     const filePath = path.resolve(__dirname, 'home.html');
     res.sendFile(filePath);
 });
+
+app.post('/send', (req,res) => {
+    console.log(req.body.uname, req.body.uphone, req.body.upassword);
+})
 
 app.listen(port, () => {
     console.log(`App listening at port http://localhost:${port}`);
