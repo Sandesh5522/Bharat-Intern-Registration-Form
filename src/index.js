30a9c8219cb7357612fdcd26762e2f5fad6f8df8
Sandesh <sandeshsutar802@gmail.com>
2024-01-01 00:38:17 +0530

    User data stored in db.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { MongoClient } = requiere('mongodb');
-const uri = "mongodb+srv://<user>:<password>@<cluster-url>?w=majority";
+const { MongoClient } = require('mongodb');
+const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 const path = require('path');
 const app = express();
@@ -10,8 +10,8 @@ const port = 3000;
 async function run() {
     try {
         await client.connect();
-        const db = client.db("myDatabase");
-        const result = await db.getCollection('user').find({});
+        const db = client.db("nodereg");
+        const result = db.collection("coll01").find({}).toArray();
         console.log(result);
     }
     finally {
@@ -20,17 +20,28 @@ async function run() {
 }
 run().catch(console.dir);
 
+function addData(db, body) {
+    var dataobj = { name: body.uname, phone: body.uphone, password: body.upassword };
+    var result = db.collection("coll01").insertOne(dataobj, function(err, res) {
+        if (err) throw err;
+        console.log("data inserted.");
+        db.close();
+    });
+}
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req,res) => {
-    // res.send('Hello World');
     const filePath = path.resolve(__dirname, 'home.html');
     res.sendFile(filePath);
 });
 
 app.post('/send', (req,res) => {
+    client.connect();
+    const db = client.db("nodereg");
     console.log(req.body.uname, req.body.uphone, req.body.upassword);
-})
+    addData(db, req.body);
+});
 
 app.listen(port, () => {
     console.log(`App listening at port http://localhost:${port}`);
